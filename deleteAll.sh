7a48243e99d05754a715e3ec0919ea952ea235d3
Daghan Acay <daghanacay@gmail.com>
2019-02-10 11:53:10 +1100

    last step with delete script

--- deleteAll.sh
+++ deleteAll.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
-# get bot ARNS
-export CHUCKBOT_FUNCTION_ARN=$(aws cloudformation describe-stacks --stack-name  $STACK_NAME_AIML --query "Stacks[0].Outputs" --region $AWS_REGION | jq -r '.[] | select(.OutputKey == "ChuckBotFunction") | .OutputValue')
-export MOVIEBOT_FUNCTION_ARN=$(aws cloudformation describe-stacks --stack-name  $STACK_NAME_AIML --query "Stacks[0].Outputs" --region $AWS_REGION | jq -r '.[] | select(.OutputKey == "MovieBotFunction") | .OutputValue')
-echo "chuckbot ARN: $CHUCKBOT_FUNCTION_ARN"
-echo "Moviebot ARN: $MOVIEBOT_FUNCTION_ARN"
-echo "Running permissions"
-# Execute the following commands to add permissions so Lex can invoke the chatbot related functions
-aws lambda add-permission --statement-id Lex --function-name $CHUCKBOT_FUNCTION_ARN --action lambda:\* --principal lex.amazonaws.com --region $AWS_REGION
-aws lambda add-permission --statement-id Lex --function-name $MOVIEBOT_FUNCTION_ARN --action lambda:\* --principal lex.amazonaws.com --region $AWS_REGION
-#Update the bots intents with the Lambda ARN:
-jq '.fulfillmentActivity.codeHook.uri = $arn' --arg arn $CHUCKBOT_FUNCTION_ARN backend/ChuckBot/intent.json -M > tmp.txt ; cp tmp.txt backend/ChuckBot/intent.json; rm tmp.txt
-jq '.fulfillmentActivity.codeHook.uri = $arn' --arg arn $MOVIEBOT_FUNCTION_ARN backend/MovieBot/intent.json -M > tmp.txt ; cp tmp.txt backend/MovieBot/intent.json; rm tmp.txt
-#  And, deploy the slot types, intents and bots
-echo "Adding lexbot"
-aws lex-models put-slot-type --cli-input-json file://backend/ChuckBot/slot-type.json --region $AWS_REGION
-aws lex-models put-intent --cli-input-json file://backend/ChuckBot/intent.json --region $AWS_REGION
-aws lex-models put-bot --cli-input-json file://backend/ChuckBot/bot.json --region $AWS_REGION
-aws lex-models put-slot-type --cli-input-json file://backend/MovieBot/slot-type.json --region $AWS_REGION
-aws lex-models put-intent --cli-input-json file://backend/MovieBot/intent.json --region $AWS_REGION
-aws lex-models put-bot --cli-input-json file://backend/MovieBot/bot.json --region $AWS_REGION
+# delete Lex bots
+aws lex-models delete-bot --name `jq -r .name backend/ChuckBot/bot.json` --region $AWS_REGION
+aws lex-models delete-bot --name `jq -r .name backend/MovieBot/bot.json` --region $AWS_REGION
+aws lex-models delete-intent --name `jq -r .name backend/ChuckBot/intent.json` --region $AWS_REGION
+aws lex-models delete-intent --name `jq -r .name backend/MovieBot/intent.json` --region $AWS_REGION
+aws lex-models delete-slot-type --name `jq -r .name backend/ChuckBot/slot-type.json` --region $AWS_REGION
+aws lex-models delete-slot-type --name `jq -r .name backend/MovieBot/slot-type.json` --region $AWS_REGION
+#Delete bot lambdas
+aws cloudformation delete-stack --stack-name $STACK_NAME_AIML --region $AWS_REGION
+
+#Delete delete the resources created by the Amplify CLI.
+
+amplify delete
